618a1db4be6a3a0ce9cf27aa9ebe28d0058c6ce5
fieryinferno <bagassetia271@gmail.com>
2023-03-01 08:22:29 +0700

    Add comment in build_push_image.sh and docker-compose.yml

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+# Ini adalah perintah untuk membuat docker image
 docker build -t item-app:v1 .
+
+# Ini adalah perintah untuk menampilkan images yang ada di local
 docker images
+
+# Ini adalah perintah untuk mengganti nama images agar sesuai dengan format docker registry
 docker tag item-app:v1 bagassetia/item-app:v1
+
+# Ini adalah perintah untuk login ke docker hub
 echo $PASSWORD_DOCKER_HUB | docker login -u bagassetia --password-stdin
+
+# Ini adalah perintah untuk menyimpan image yang sudah dibuat ke docker registry
 docker push bagassetia/item-app:v1
