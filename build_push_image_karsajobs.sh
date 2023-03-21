139f8051bd76c3318c3337f97acf9faabcc615e4
FieryInferno <bagassetia271@gmail.com>
2023-03-21 07:38:44 +0700

    Add comment

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+
+# Perintah untuk build image dari dockerfile
 docker build -t bagassetia/karsajobs:latest .
+
+# Perintah untuk login ke docker hub dengan menggunakan variable password yang sudah disimpan
 echo $PASSWORD_DOCKER_HUB | docker login -u bagassetia --password-stdin
+
+# Perintah untuk push image yang sudah dibuild ke docker hub
 docker push bagassetia/karsajobs:latest
